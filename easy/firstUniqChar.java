f5c240b775e73e47c778d952681988399970b0b1
Jinglong Guo <guojlong6@gmail.com>
2016-12-19 12:32:23 -0500

    update some other methods of each questions

--- easy/firstUniqChar.java
+++ easy/firstUniqChar.java
@@ -33,3 +33,22 @@ public class Solution {
         return -1;
     }
 }
+
+/**
+ * running time complexity: O(n)
+ * top method. 
+ */
+public class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        int i;
+        for(i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+        for(i = 0; i < s.length(); i++) {
+            if(freq[s.charAt(i) - 'a'] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
