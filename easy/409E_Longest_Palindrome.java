92786604dff18e1b940daa4e71d43326c60e49e6
guollong <jinglong@andrew.cmu.edu>
2017-05-29 16:50:55 -0400

    Update 409E_Longest_Palindrome.java

--- easy/409E_Longest_Palindrome.java
+++ easy/409E_Longest_Palindrome.java
@@ -9,10 +9,17 @@
  * Author: Jinglong Guo
  * Difficulty: Easy; Company: .
  * Date: 03/25/2017
+ * Modified data: 05/29/2017
  */
 
+// Original wrong idea: take all even frequencies and the largest odd frequency of the characters.
+// Correct: There could be only one character which has odd frequency. For other odd-frequency characters, take even numbers
+//      from them. For even-frequency characters, take them all.
+// Notes: Methods counting the frequency of the characters: integer array(length=26 or 52(case sensative)), hashmap.
 // 如果output不等于输入字符串的长度，说明有的字母有奇数个，而回文中只允许有一个个数为奇数的字母存在。
 // 回文：正读和反渎是一样的。
+
+// Integer array to take the frequency of characters.
 public class Solution {
     public int longestPalindrome(String s) {
         int[] frequency = new int[26 * 2];
@@ -34,4 +41,28 @@ public class Solution {
         return output == s.length() ? output : output + 1;
     }
 }
+
+// HashMap to take the frequency of characters.
+public class Solution {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char temp = s.charAt(i);
+            if (map.containsKey(temp)) {
+                map.put(temp, map.get(temp) + 1);
+            } else {
+                map.put(temp, 1);
+            }
+        }
+        int sum = 0;
+        for (int freq : map.values()) {
+            sum += (freq % 2 == 0 ? freq : freq - 1);
+        }
+        return sum == s.length() ? sum : sum + 1;
+    }
+}
+
+
+
+
 
