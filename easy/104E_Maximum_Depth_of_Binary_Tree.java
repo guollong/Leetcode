4506b4cea29f688b22740ad672c095593d6f770f
guollong <jinglong@andrew.cmu.edu>
2017-05-30 15:26:52 -0400

    Update 104E_Maximum_Depth_of_Binary_Tree.java

--- easy/104E_Maximum_Depth_of_Binary_Tree.java
+++ easy/104E_Maximum_Depth_of_Binary_Tree.java
@@ -1,11 +1,12 @@
 /**
  * Question description: Given a binary tree, find its maximum depth.
- * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *  The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
  */
 
 /**
  * Author: Jinglong Guo
- * Difficulty: Easy; Company: .
+ * Difficulty: Easy; 
+ * Company: .
  * Date: 03/02/2017
  */
 
@@ -19,24 +20,6 @@
  * }
  */
 
-// my solution. 
-public class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return depthHelper(root);
-    }
-    
-    private int depthHelper(TreeNode node) {
-        if (node == null) {
-            return 0;
-        }
-        return Math.max(depthHelper(node.left), depthHelper(node.right)) + 1;
-    }
-}
-
-// more simpler solution.
 public class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) {
