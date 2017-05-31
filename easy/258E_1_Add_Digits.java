d87e109e192734384d8e0f152c0203efbbe54d26
guollong <jinglong@andrew.cmu.edu>
2017-05-31 15:10:12 -0400

    Update and rename 258E_Add_Digits.java to 258E_1_Add_Digits.java

--- easy/258E_1_Add_Digits.java
+++ easy/258E_1_Add_Digits.java
@@ -4,8 +4,6 @@
  * For example, the digital root of 65,536 is 7, because 6 + 5 + 5 + 3 + 6 = 25 and 2 + 5 = 7.
  */
 
-// 很巧妙。。。。。。。。。。。。
-
 /**
  * Question description: Calculate the digital root of one non-negative number.
  * Example: Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
@@ -13,13 +11,33 @@
 
 /**
  * Author: Jinglong Guo
- * Difficulty: Easy; Company: .
+ * Difficulty: Easy; 
+ * Company: .
  * Date: 03/08/2017
  */
 
+// Approach 1: O(1) method.
 public class Solution {
     public int addDigits(int num) {
         return num - 9 * ((num - 1) / 9);
-        // return 1 + (num - 1) % 9;
     }
 }
+
+// Approach 2: General recursive method.
+public class Solution {
+    public int addDigits(int num) {
+        if (num < 10) {
+            return num;
+        }
+        int count = 0;
+        int temp = num;
+        while (temp != 0) {
+            count += temp % 10;
+            temp = temp / 10;
+        }
+        return addDigits(count);
+    }
+}
+
+
+
