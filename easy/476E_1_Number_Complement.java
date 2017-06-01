b1b42ec2c423316d4e9b129c8be9204f95cdb056
guollong <jinglong@andrew.cmu.edu>
2017-06-01 11:02:45 -0400

    Update and rename 476E_Number_Complement.java to 476E_1_Number_Complement.java

--- easy/476E_1_Number_Complement.java
+++ easy/476E_1_Number_Complement.java
@@ -4,42 +4,23 @@
  * Note: The given integer is guaranteed to fit within the range of a 32-bit signed integer.
  *    The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2. If it does not exist, output -1 for this number.
  * 
- * Example 1:
- * Input: 5; Output: 2
+ * Example 1: Input: 5; Output: 2
  * Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
- * Example 2:
- * Input: 1; Output: 0
+ * Example 2: Input: 1; Output: 0
  * Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
  */
 
 /**
  * Author: Jinglong Guo
- * Difficulty: Easy; Company: .
+ * Difficulty: Easy; 
+ * Company: .
  * Date: 02/24/2017
  */
 
-// 求补码.
-// Jinglong's solution.
 public class Solution {
     public int findComplement(int num) {
-        int count = 0;
-        int temp = num;
-        // how many bits you need to represent this number??
-        while (temp != 0) {
-            temp = temp >> 1;
-            count = count + 1;
-        }
-        return (1 << count) - 1 - num;
-    }
-}
-
-
-// solution from discussion.
-public class Solution {
-    public int findComplement(int num) {
-        int mask = (Integer.highestOneBit(num) << 1) - 1;
-        num = ~num;
-        return num & mask;
+        return ~num & ((Integer.highestOneBit(num) << 1) - 1);
+        // return (Integer.highestOneBit(num) << 1) - 1 - num;
     }
 }
 
