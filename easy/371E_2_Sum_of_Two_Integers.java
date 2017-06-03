08c38c24f2678203acb6360e135398bdaaec7b93
Jinglong Guo <guojlong6@gmail.com>
2017-06-02 22:30:44 -0400

    commit 3 questions

---
+++ easy/371E_2_Sum_of_Two_Integers.java
@@ -0,0 +1,76 @@
+/**
+ * Question description: Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ * 
+ * Example: Given a = 1 and b = 2, return 3.
+ */
+
+/**
+ * Author: Jinglong Guo
+ * Difficulty: Easy; 
+ * Company: .
+ */
+
+/**
+ * Progress...
+ * Create Date: 06/02/2017
+ */
+
+
+// Steps: 1. First, we can use "and"("&") operation between a and b to find a carry.
+//		  2. carry = a & b, then carry = 0001
+//		  3. Second, we can use "xor" ("^") operation between a and b to find the different bit, and assign it to a,
+// 		  4. Then, we shift carry one position left and assign it to b, b = 0010.
+// 		  5. Iterate until there is no carry (or b == 0)
+
+// 照目前的情况来看，只能先记住。。。（无奈）
+
+// Iterative
+public int getSum(int a, int b) {
+	if (a == 0) return b;
+	if (b == 0) return a;
+
+	while (b != 0) {
+		int carry = a & b;
+		a = a ^ b;
+		b = carry << 1;
+	}
+	
+	return a;
+}
+
+// Recursive
+public int getSum(int a, int b) {
+	return (b == 0) ? a : getSum(a ^ b, (a & b) << 1);
+}
+
+// Iterative
+public int getSubtract(int a, int b) {
+	while (b != 0) {
+		int borrow = (~a) & b;
+		a = a ^ b;
+		b = borrow << 1;
+	}
+	
+	return a;
+}
+
+// Recursive
+public int getSubtract(int a, int b) {
+	return (b == 0) ? a : getSubtract(a ^ b, (~a & b) << 1);
+}
+
+// Get negative number
+public int negate(int x) {
+	return ~x + 1;
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ easy/572E_2_Subtree_of_Another_Tree.java
@@ -0,0 +1,64 @@
+/**
+ * Question description: Given two non-empty binary trees s and t, check whether tree t has 
+ *		exactly the same structure and node values with a subtree of s. A subtree of s is a 
+ *		tree consists of a node in s and all of this node's descendants. The tree s could 
+ *		also be considered as a subtree of itself.
+ *
+ * Example: 
+ *	Given tree s:
+ *	    3
+ *	   / \
+ *	  4   5
+ *	 / \
+ *	1   2
+ * Given tree t:
+ *	  4 
+ *	 / \
+ *	1   2
+ * Return true, because t has the same structure and node values with a subtree of s.
+ */
+
+/**
+ * Author: Jinglong Guo
+ * Difficulty: Easy; 
+ * Company: .
+ */
+
+/**
+ * Progress...
+ * Create Date: 06/02/2017
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        if (isSameTree(s, t)) {
+            return true;
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    private boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (t == null || s == null || s.val != t.val) {
+            return false;
+        }
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+}
+
+
+
