ad9798cb04eddba215965fdecbf218010b5dc8e7
guollong <jinglong@andrew.cmu.edu>
2017-06-05 17:37:02 -0400

    Update and rename 459E_Repeated_Substring_Pattern.java to 459E_2_Repeated_Substring_Pattern.java

--- easy/459E_2_Repeated_Substring_Pattern.java
+++ easy/459E_2_Repeated_Substring_Pattern.java
@@ -7,13 +7,19 @@
  * Example 2: Input: "aba", Output: False;
  */
 
+
+// There is also a complicate algorithm called KMP to solve this problem. (But just need to know that.)
+// KMP algorithm: searches for occurrences of a "word" W within a main "text string" S
+
 /**
  * Author: Jinglong Guo
- * Difficulty: Easy; Company: .
- * Date: 04/03/2017
+ * Difficulty: Easy; 
+ * Company: .
+ * Create Date: 04/03/2017
+ * Update date: 06/05/2017
  */
 
-// The similiar idea with me, but cost less time.
+// My solution.
 public class Solution {
     public boolean repeatedSubstringPattern(String s) {
         int length = s.length();
@@ -35,10 +41,27 @@ public class Solution {
     }
 }
 
-// There is also a complicate algorithm called KMP to solve this problem.
-// KMP algorithm: searches for occurrences of a "word" W within a main "text string" S
-
-
+// The similiar idea with me, but use stringbuilder to check the whole string.
+public class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int length = s.length();
+        int halflength = s.length() / 2;
+        for (int i = halflength; i >= 1; i--) {
+            if (length % i == 0) {
+                int count = length / i;
+                String sub = s.substring(0, i);
+                StringBuilder test = new StringBuilder();
+                for (int j = 0; j < count; j++) {
+                    test.append(sub);
+                }
+                if (test.toString().equals(s)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
 
 
 
