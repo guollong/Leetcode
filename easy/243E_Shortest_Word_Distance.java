758ab3c011ecb0f1d7395c4d313a8f1d559dd103
Jinglong Guo <guojlong6@gmail.com>
2017-06-13 14:40:38 -0400

    commit easy question

--- easy/243E_Shortest_Word_Distance.java
+++ easy/243E_Shortest_Word_Distance.java
@@ -7,38 +7,38 @@
 
 /**
  * Author: Jinglong Guo
- * Difficulty: Easy; Company: Linkedin.
- * Date: 02/02/2017
- * Notes: should take duplicate into consideration.
+ * Difficulty: Easy; 
+ * Company: Linkedin.
+ * Create Date: 02/02/2017
+ * Update date: 06/13/2017
  */
 
+// Solution 1: Loop over the string array, update the min distance only when the indices are changed.
+//      faster than next simpler version.
 public class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
-        List<Integer> index1 = new ArrayList<Integer>();
-        List<Integer> index2 = new ArrayList<Integer>();
-        List<Integer> distance = new ArrayList<Integer>();
+        int index1 = -1;
+        int index2 = -1;
+        int minDistance = Integer.MAX_VALUE;
         for (int i = 0; i < words.length; i++) {
             if (words[i].equals(word1)) {
-                index1.add(i);
+                index1 = i;
+                if (index2 != -1 && minDistance > Math.abs(index1 - index2)) {
+                    minDistance = Math.abs(index1 - index2);
+                }
             }
             if (words[i].equals(word2)) {
-                index2.add(i);
+                index2 = i;
+                if (index1 != -1 && minDistance > Math.abs(index1 - index2)) {
+                    minDistance = Math.abs(index1 - index2);
+                }
             }
         }
-        for (Integer i : index1) {
-            for (Integer j : index2) {
-                distance.add(Math.abs(i - j));
-            }
-        }
-        Collections.sort(distance);
-        return distance.get(0);
+        return minDistance;
     }
 }
 
-/**
- * Improvement: For each iteration, updates the index1 and index2, when the distance get 
- * 				smaller, assign the smaller value to the min value.
- */
+// Simpler version. 
 public class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
         int index1 = -1, index2 = -1, min = Integer.MAX_VALUE;
