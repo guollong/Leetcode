27f980a521aa627ceb28a44b4ceb6f1bfa891912
guollong <jinglong@andrew.cmu.edu>
2017-06-14 15:03:58 -0400

    Create 107E_BinaryTree_LevelOrder_Traversal.java

--- easy/107E_BinaryTree_LevelOrder_Traversal.java
+++ easy/107E_BinaryTree_LevelOrder_Traversal.java
@@ -4,7 +4,7 @@
  * Example 1: Given binary tree [3,9,20,null,null,15,7],
  *     3
  *    / \
- * 	 9  20
+ *   9  20
  *     /  \
  *    15   7
  * return its bottom-up level order traversal as: [[15,7], [9,20],[3]]
