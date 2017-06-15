8febbfe2628e51c905b4900b76016c920ac94fc5
Jinglong Guo <guojlong6@gmail.com>
2017-06-15 19:22:29 -0400

    06.15 commit

--- easy/170E_3_Two_Sum_III.java
+++ easy/170E_3_Two_Sum_III.java
@@ -1,6 +1,7 @@
 /**
  * Author: Jinglong Guo
- * Difficulty: Easy; Company: Linkedin.
+ * Difficulty: Easy; 
+ * Company: Linkedin.
  * Date: 02/03/2017
  * Notes: always take big data into consideration. O(n^2) is the worst case.
  */
@@ -45,37 +46,6 @@ public class TwoSum {
             int anotherEle = value - element;
             if ((element != anotherEle && data.containsKey(anotherEle)) || (anotherEle == element && data.get(element) != 1)){
                 return true;
-            }
-        }
-        return false;
-    }
-}
-
-////////////////////////////////////////////////////////
-// 超时的答案，尽量不写复杂度为n^2的程序。Time Limit Exceeded
-////////////////////////////////////////////////////////
-public class TwoSum {
-    
-    List<Integer> data;
-    /** Initialize your data structure here. */
-    public TwoSum() {
-        data = new ArrayList<Integer>();
-    }
-    
-    /** Add the number to an internal data structure.. */
-    // Running time complexity: amortized O(1)
-    public void add(int number) {
-        data.add(number);
-    }
-    
-    /** Find if there exists any pair of numbers which sum is equal to the value. */
-    // Running time complexity: O(n^2)
-    public boolean find(int value) {
-        for (int i = 0; i < data.size() - 1; i++) {
-            for (int j = i + 1; j < data.size(); j++) {
-                if (data.get(i) + data.get(j) == value) {
-                    return true;
-                }
             }
         }
         return false;
---
+++ easy/270E_3_Closest_Binary_Search_Tree_Value.java
@@ -0,0 +1,58 @@
+/**
+ * Question description: Given a non-empty binary search tree and a target value, 
+ *		find the value in the BST that is closest to the target.
+ * 
+ * Note: Given target value is a floating point.
+ */
+
+/**
+ * Author: Jinglong Guo
+ * Difficulty: Easy; 
+ * Company: .
+ */
+
+/**
+ * Progress...
+ * Create Date: 06/15/2017
+ */
+
+// Solution 1: Iterative method.
+public class Solution {
+    public int closestValue(TreeNode root, double target) {
+        double min = Double.MAX_VALUE;
+        TreeNode temp = root;
+        int result = root.val;
+        while (temp != null) {
+            if (temp.val == target) {
+                return temp.val;
+            }
+            if (min > Math.abs(target - temp.val)) {
+                min = Math.abs(target - temp.val);
+                result = temp.val;
+            }
+            if (temp.val * 1.0 > target) {
+                temp = temp.left;
+            } else {
+                temp = temp.right;
+            }
+        }
+        return result;
+    }
+}
+
+// Solution 2: Recursive method.
+public class Solution {
+    public int closestValue(TreeNode root, double target) {
+    	// Need to determine which node to compare with. 
+        TreeNode child = root.val > target ? root.left : root.right;
+        if (child == null) {
+            return root.val;
+        }
+        int closestChild = closestValue(child, target);
+        return Math.abs(root.val - target) < Math.abs(closestChild - target) ? root.val : closestChild;
+    }
+}
+
+
+
+
