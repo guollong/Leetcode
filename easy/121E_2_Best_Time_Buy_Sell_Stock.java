82578b2d279e1a3bf8301f4f82b983e050473a3f
guollong <jinglong@andrew.cmu.edu>
2017-07-07 10:21:34 -0400

    Update 121E_2_Best_Time_Buy_Sell_Stock.java

--- easy/121E_2_Best_Time_Buy_Sell_Stock.java
+++ easy/121E_2_Best_Time_Buy_Sell_Stock.java
@@ -22,8 +22,9 @@
 //      and find a contiguous subarray giving maximum profit. If the difference falls below 0, reset it to zero.
 public int maxProfit(int[] prices) {
     int maxCur = 0, maxSoFar = 0;
-    for(int i = 1; i < prices.length; i++) {
-        maxCur = Math.max(0, maxCur += prices[i] - prices[i-1]);
+    for (int i = 1; i < prices.length; i++) {
+        maxCur += prices[i] - prices[i-1];
+        maxCur = Math.max(0, maxCur);
         maxSoFar = Math.max(maxCur, maxSoFar);
     }
     return maxSoFar;
