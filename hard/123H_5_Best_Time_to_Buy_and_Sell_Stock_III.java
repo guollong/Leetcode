39e8d6df3fbca701efacd4b71c4b1d3cca2e69ea
guollong <jinglong@andrew.cmu.edu>
2017-07-24 10:38:41 -0700

    Update 123H_5_Best_Time_to_Buy_and_Sell_Stock_III.java

--- hard/123H_5_Best_Time_to_Buy_and_Sell_Stock_III.java
+++ hard/123H_5_Best_Time_to_Buy_and_Sell_Stock_III.java
@@ -42,7 +42,7 @@ public class Solution {
 /**
  * dp[i, j] represents the max profit up until prices[j] using at most i transactions. 
  * dp[i, j] = max(dp[i, j-1], prices[j] - prices[jj] + dp[i-1, jj]) { jj in range of [0, j-1] }
- *          = max(dp[i, j-1], prices[j] + max(dp[i-1, jj] - prices[jj]))
+ *          = max(dp[i, j-1], prices[j] + max(localMax, dp[i-1, j] - prices[j]))
  * dp[0, j] = 0; 0 transactions makes 0 profit
  * dp[i, 0] = 0; if there is only one price data point you can't make any transaction.
  */
