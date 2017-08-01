ccf47c64513324d68c7f5b592e49c733a309f439
guollong <jinglong@andrew.cmu.edu>
2017-08-01 16:18:15 -0700

    Update 101E_3_Symmetric_Tree.java

--- easy/101E_3_Symmetric_Tree.java
+++ easy/101E_3_Symmetric_Tree.java
@@ -14,7 +14,6 @@
  *		   \   \
  *		   3    3
  * Follow up: Bonus points for both recursive method and iterative method.
- * 但是我只完成了一种。。。。。。。。。。。。。。。。
  */
 
 /**
@@ -26,6 +25,7 @@
 /**
  * Progress...
  * Create Date: 06/14/2017
+ * Modified Date: 08/01/2017
  */
 
 /**
@@ -37,6 +37,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// Solution 1: Recursive method.
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
         if (root == null) {
@@ -56,4 +57,54 @@ public class Solution {
     }
 }
 
-
+// Solution 2: DFS: Using stack. Iniitially, push the left and right child of the root to the stack. 
+//      After that, every time pop two nodes from stack and check whether they are the same, at the 
+//      same time push the left child of the left node into the stack, push the right child of the 
+//      right node to the stack, if one of these two nodes does not exist, return false, push the right 
+//      child of the left node to the stack, push the left child of the right node to the stack, if 
+//      one of these two nodes does not exist, return false.
+public class Solution {
+    private Stack<TreeNode> stack = new Stack<>();
+    
+    public boolean isSymmetric(TreeNode root) {
+        // corner case.
+        if (root == null) {
+            return true;
+        }
+        
+        
+        // Push the left and right child to the stack.
+        if (!pushChildrenSuccess(root.left, root.right)) {
+            return false;
+        }
+        
+        // Iterative part.
+        while (!stack.empty()) {
+            TreeNode right = stack.pop();
+            TreeNode left = stack.pop();
+            
+            if (left.val != right.val) {
+                return false;
+            }
+            if (!pushChildrenSuccess(left.left, right.right)) {
+                return false;
+            }
+            if (!pushChildrenSuccess(left.right, right.left)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean pushChildrenSuccess(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 != null && node2 != null) {
+            stack.push(node1);
+            stack.push(node2);
+            return true;
+        }
+        return false;
+    }
+}
