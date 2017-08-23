6f5b9a2f9401ac176e5681fb0009b32b72aa150e
Jinglong Guo <guojlong6@gmail.com>
2017-08-22 23:09:26 -0700

    08.22 commit

---
+++ Algorithms/48M_6_Rotate_Image.java
@@ -0,0 +1,26 @@
+/**
+ * Question description: Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+ *		If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+ * 
+ * Note: The replacement must be in-place, do not allocate extra memory.
+ * 
+ * Examples: 
+ *	1,2,3 → 1,3,2
+ *	3,2,1 → 1,2,3
+ *	1,1,5 → 1,5,1
+ *
+ */
+
+/**
+ * Author: Jinglong Guo
+ * Difficulty: Medium; 
+ * Company: .
+ */
+
+/**
+ * Progress...
+ * Create Date: 08/22/2017
+ */
+
+
+
