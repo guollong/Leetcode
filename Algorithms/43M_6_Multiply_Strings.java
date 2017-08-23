6084ba2cb90d83ebf0671205393a6c9a65bc2d5b
Jinglong Guo <guojlong6@gmail.com>
2017-08-23 10:45:05 -0700

    08.23 commit

--- Algorithms/43M_6_Multiply_Strings.java
+++ Algorithms/43M_6_Multiply_Strings.java
@@ -15,8 +15,40 @@
 
 /**
  * Progress...
- * Create Date: 08/22/2017
+ * Create Date: 08/23/2017
  */
+
+class Solution {
+    // Start from right to left, perform multiplication on every pair of digits, and add them together.
+    // 'num1[i] * num2[j]' will be placed at indices '[i + j, i + j + 1]'
+    public String multiply(String num1, String num2) {
+        int length1 = num1.length();
+        int length2 = num2.length();
+        int[] pos = new int[length1 + length2];
+        
+        for (int i = length1 - 1; i >= 0; i--) {
+            for (int j = length2 -1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j;
+                int p2 = i + j + 1;
+                int sum = mul + pos[p2];
+                
+                pos[p1] += sum / 10;
+                pos[p2] = sum % 10;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int p : pos) {
+            // The first number must be greater than 0.
+            if (sb.length() != 0 || p != 0) {
+                sb.append(p);
+            }
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
+
 
 
 
--- Algorithms/48M_6_Rotate_Image.java
+++ Algorithms/48M_6_Rotate_Image.java
@@ -16,5 +16,37 @@
  * Create Date: 08/22/2017
  */
 
-
+// Clockwise rotation: Flip the matrix based on diagonal and then the middle column;
+// AntiClockwise rotation: Flip the matrix based on anti-diagonal and then the middle column.
+class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        int row = matrix.length;
+        int column = matrix[0].length;
+        
+        // flip the matrix symmetrically based on diagonal.
+        // Attention: Only loop half of the matrix to swap, otherwise, if loop over the whole matrix, 
+        //            the matrix will return to the original one.
+        for (int i = 0; i < row; i++) {
+            for (int j = i + 1; j < column; j++) {
+                swap(matrix, i, j, j, i);
+            }
+        }
+        
+        // flip the matrix based on middle column;
+        for (int j = 0; j < column / 2; j++) {
+            for (int i = 0; i < row; i++) {
+                swap(matrix, i, j, i, column - j - 1);
+            }
+        }
+    }
+    
+    private void swap(int[][] matrix, int row1, int column1, int row2, int column2) {
+        int temp = matrix[row1][column1];
+        matrix[row1][column1] = matrix[row2][column2];
+        matrix[row2][column2] = temp;
+    }
+}
 
