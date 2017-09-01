92b05bb4669bb64efacd7a2a9a90ed92963c3908
guollong <jinglong@andrew.cmu.edu>
2017-08-31 22:45:05 -0700

    08.31 update

--- Algorithms/220M_6_Contains_Duplicate_III.java
+++ Algorithms/220M_6_Contains_Duplicate_III.java
@@ -13,9 +13,13 @@
 /**
  * Progress...
  * Create Date: 08/09/2017
+ * Update Date: 08/31/2017
  */
 
 // Solution 1: Balanced binary search tree. (TreeSet)
+// Idea: Always keep k elements in the treeset, for each element, get the ceil which is the smallest value greater
+//      than the current element and the floor which is the largest value less than the current element, check 
+//      if they are within the range of t around the current element.
 // Running time complexity: O(n * logk).
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
@@ -43,6 +47,7 @@ public class Solution {
 
 // Solution 2: Using bucket. (inspired by bucket sort)
 // Each bucket could only have one element, so we use hashmap to keep this, the key is the id of the bucket.
+// Use bucket to store the ceil and floor of the current element.
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         if (t < 0 || k <= 0) {
@@ -73,24 +78,10 @@ public class Solution {
     
     // Get the ID of the bucket from the element x and bucket width k.
     private int getID(int x, int width) {
+        // 如果x小于0，问了保证bucket的连贯，id=(x + 1) / width - 1；
         return x < 0 ? (x + 1) / width - 1 : x / width;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
