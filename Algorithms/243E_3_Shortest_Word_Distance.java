e27094659e90c77f9a0193cb673fd8880981fd05
guollong <jinglong@andrew.cmu.edu>
2017-09-03 16:01:57 -0700

    09.03 update

--- Algorithms/243E_3_Shortest_Word_Distance.java
+++ Algorithms/243E_3_Shortest_Word_Distance.java
@@ -11,11 +11,12 @@
  * Company: Linkedin.
  * Create Date: 02/02/2017
  * Update date: 06/13/2017
+ * Update date: 09/03/2017
  */
 
 // Solution 1: Loop over the string array, update the min distance only when the indices are changed.
 //      faster than next simpler version.
-public class Solution {
+class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
         int index1 = -1;
         int index2 = -1;
@@ -23,14 +24,14 @@ public class Solution {
         for (int i = 0; i < words.length; i++) {
             if (words[i].equals(word1)) {
                 index1 = i;
-                if (index2 != -1 && minDistance > Math.abs(index1 - index2)) {
-                    minDistance = Math.abs(index1 - index2);
+                if (index2 != -1) {
+                    minDistance = Math.min(minDistance, Math.abs(index1 - index2));
                 }
             }
             if (words[i].equals(word2)) {
                 index2 = i;
-                if (index1 != -1 && minDistance > Math.abs(index1 - index2)) {
-                    minDistance = Math.abs(index1 - index2);
+                if (index1 != -1) {
+                    minDistance = Math.min(minDistance, Math.abs(index1 - index2));
                 }
             }
         }
@@ -39,20 +40,21 @@ public class Solution {
 }
 
 // Simpler version. 
-public class Solution {
+class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
-        int index1 = -1, index2 = -1, min = Integer.MAX_VALUE;
-        
+        int index1 = -1;
+        int index2 = -1;
+        int minDistance = Integer.MAX_VALUE;
         for (int i = 0; i < words.length; i++) {
-            if (words[i].equals(word1)) 
+            if (words[i].equals(word1)) {
                 index1 = i;
-    
-            if (words[i].equals(word2)) 
+            } else if (words[i].equals(word2)) {
                 index2 = i;
-                
-            if (index1 != -1 && index2 != -1 && Math.abs(index1 - index2) < min)
-                min = Math.abs(index1 - index2);
+            }
+            if (index1 != -1 && index2 != -1) {
+                minDistance = Math.min(minDistance, Math.abs(index1 - index2));
+            }
         }
-        return min;
+        return minDistance;
     }
 }
