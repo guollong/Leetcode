cf51cffbc7daa2dd725d4ffc42cf7ee781674a73
guollong <jinglong@andrew.cmu.edu>
2017-09-03 18:22:54 -0700

    09.03 update

--- Algorithms/246E_3_Strobogrammatic_Number.java
+++ Algorithms/246E_3_Strobogrammatic_Number.java
@@ -11,13 +11,16 @@
  * Company: Google.
  * Create Date: 03/28/2017
  * Update date: 06/13/2017
+ * Update Date: 09/03/2017
  */
 
 // 是某种程度上的对称，所以除了其本身是对称的数字以外，还要求在对称位置上的值相等，（6和9除外，要互为对方的值）。
+
+// Solution 1: space complexity: O(1)
 public class Solution {
     public boolean isStrobogrammatic(String num) {
         int length = num.length();
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i <= length / 2; i++) {
             int number = num.charAt(i) - '0';
             if (number == 2 || number == 3 || number == 4 || number == 5 || number == 7) {
                 return false;
@@ -36,3 +39,28 @@ public class Solution {
         return true;
     }
 }
+
+// Solution 2: space complexity: O(n)
+class Solution {
+    public boolean isStrobogrammatic(String num) {
+        List<Character> stroboChar = Arrays.asList('0','1', '6', '8', '9');
+        StringBuilder strobo = new StringBuilder();
+        for (int i = 0; i < num.length(); i++) {
+            if (!stroboChar.contains(num.charAt(i))) {
+                return false;
+            } else if (num.charAt(i) == '6') {
+                strobo.append('9');
+            } else if (num.charAt(i) == '9') {
+                strobo.append('6');
+            } else {
+                strobo.append(num.charAt(i));
+            }
+        }
+        return strobo.reverse().toString().equals(num);
+    }
+}
+
+
+
+
+
