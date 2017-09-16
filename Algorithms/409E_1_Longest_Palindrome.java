5b4dad6200c7a78e484b0b6d8c06cd806ffca947
guollong <jinglong@andrew.cmu.edu>
2017-09-15 18:10:30 -0700

    09.15 update

--- Algorithms/409E_1_Longest_Palindrome.java
+++ Algorithms/409E_1_Longest_Palindrome.java
@@ -9,19 +9,12 @@
  * Author: Jinglong Guo
  * Difficulty: Easy; 
  * Company: Google.
- * Date: 03/25/2017
- * Modified data: 05/29/2017
+ * Create Date: 03/25/2017
+ * Update data: 05/29/2017
+ * Update date: 09/15/2017
  */
 
-// Original wrong idea: take all even frequencies and the largest odd frequency of the characters.
-// Correct: There could be only one character which has odd frequency. For other odd-frequency characters, take even numbers
-//      from them. For even-frequency characters, take them all.
-// Notes: Methods counting the frequency of the characters: integer array(length=26 or 52(case sensative)), hashmap.
-// 如果output不等于输入字符串的长度，说明有的字母有奇数个，而回文中只允许有一个个数为奇数的字母存在。
-// 回文：正读和反渎是一样的。
-
-// 2 approaches: alphabet array, hashmap.
-// Integer array to take the frequency of characters.
+// Solution 1: alphabet array: Integer array to take the frequency of characters.
 public class Solution {
     public int longestPalindrome(String s) {
         int[] frequency = new int[26 * 2];
@@ -44,7 +37,7 @@ public class Solution {
     }
 }
 
-// HashMap to take the frequency of characters.
+// Solution 2: HashMap to take the frequency of characters.
 public class Solution {
     public int longestPalindrome(String s) {
         Map<Character, Integer> map = new HashMap<>();
